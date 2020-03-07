89e6cb60a220386a27a85f48a1a40fe616d1826a
Jonatan <jonatan@routing.is>
2020-03-07 00:11:02 +0000

    scapy first test

---
+++ scapy-sendsyns.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''Purpose:
+Just being able to send a SYN packet with scapy, to test random stuff.
+Currently has issues as it requires root permission
+
+'''
+
+import scapy.all as scapy
+import random
+
+
+
+
+def sendsyns(ip, count, port):
+    counter = 0
+    while counter < count:
+        counter += 1
+        sourceport = random.randint(3000,8000)
+        scapy.send(scapy.IP(dst=ip)/scapy.TCP(dport=port, sport=sourceport))
+
+
