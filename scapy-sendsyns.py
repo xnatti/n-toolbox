33619d63b95763230efa14aa779d4d2a36c94680
Jonatan <jonatan@routing.is>
2020-03-07 00:15:34 +0000

    fixing description

--- scapy-sendsyns.py
+++ scapy-sendsyns.py
@@ -9,7 +9,16 @@ Currently has issues as it requires root permission
 import scapy.all as scapy
 import random
 
+print('\nSyntax is as....')
+print('sendsyns(<ip address>, <dst port>, <count>\n')
 
+
+# short reference https://packetlife.net/media/library/36/scapy.pdf
+
+
+# a while loop that takes IP, port and a count as an argument
+# will send TCP SYN packets to IP:port four count number of tims.
+# will not wait for a return packet.
 
 
 def sendsyns(ip, port, count):
